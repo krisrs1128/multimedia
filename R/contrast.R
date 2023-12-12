fd597680da75fdb712fc07b3a80b55c4478d859d
Kris Sankaran <sankaran.kris@gmail.com>
2023-12-12 10:21:46 -0600

    Wrapper for estimating direct effects

--- R/contrast.R
+++ R/contrast.R
@@ -1,7 +1,72 @@
-
-contrast_means <- function(model, profile1, profile2) {
+contrast_predictions <- function(model, profile1, profile2) {
+  y_hat_1 <- predict(model, profile1)
+  y_hat_2 <- predict(model, profile2)
+  path_difference(y_hat_1, y_hat_2)
 }
 
 contrast_samples <- function(model, profile1, profile2) {
+  y1 <- sample(model, profile1)
+  y2 <- sample(model, profile2)
+  path_difference(y1, y2)
+}
+
+path_difference <- function(y1, y2) {
+  list(
+    mediators = y1$mediators - y2$mediators,
+    outcomes = y1$outcomes - y2$outcomes
+  )
+}
+
+adjust_defaults <- function(model, t1 = NULL, t2 = NULL, t_mediator = NULL) {
+  if (is.null(t1) | is.null(t2)) {
+    unique_treatments <- unique(setup_profile(model)@t_outcome[[1]])
+    t1 <- unique_treatments[1, ]
+    t2 <- unique_treatments[2, ]
+  }
+
+  if (is.character(t1) | is.character(t2)) {
+    t1 <- tibble(factor(t1))
+    t2 <- tibble(factor(t2))
+    names(t1) <- treatments(model)[1]
+    names(t2) <- treatments(model)[1]
+  }
+
+  if (is.null(t_mediator)) {
+    t_mediator <- list(t1, t2)
+    names(t_mediator) <- c(t1[[1]], t2[[1]])
+  }
+
+  list(t1 = t1, t2 = t2, t_mediator = t_mediator)
+}
+
+direct_effect <- function(model, t1 = NULL, t2 = NULL, t_mediator = NULL) {
+  t_prep <- adjust_defaults(model, t1, t2)
+  t1 <- t_prep$t1
+  t2 <- t_prep$t2
+  t_mediator <- t_prep$t_mediator
+
+  result <- list()
+  for (i in seq_along(t_mediator)) {
+    profile1 <- setup_profile(model, t_mediator[[i]], t1)
+    profile2 <- setup_profile(model, t_mediator[[i]], t2)
+    result[[i]] <- contrast_predictions(model, profile1, profile2)[["outcomes"]] |>
+      pivot_longer(everything(), names_to = "outcome", values_to = "direct_effect") |>
+      mutate(contrast = glue("{apply(t1, 1, paste0, collapse=',')} - {apply(t2, 1, paste0, collapse=',')}"))
+  }
+
+  if (is.null(names(t_mediator))) {
+    names(t_mediator) <- seq_along(t_mediator)
+  }
+
+  bind_rows(result, .id = "mediator_setting") |>
+    mutate(mediator_setting = names(t_mediator)[as.integer(mediator_setting)]) |>
+    select(outcome, mediator_setting, contrast, direct_effect)
+}
+
+indirect_pathwise <- function(model) {
+
+}
+
+indirect_effect <- function(model) {
 
 }
