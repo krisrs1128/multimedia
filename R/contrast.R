519e8827f3cf61fdc5dcff6f8db550ddf4cda9b4
Kris Sankaran <sankaran.kris@gmail.com>
2023-12-12 10:28:27 -0600

    definition for overall indirect effect

--- R/contrast.R
+++ R/contrast.R
@@ -17,7 +17,8 @@ path_difference <- function(y1, y2) {
   )
 }
 
-adjust_defaults <- function(model, t1 = NULL, t2 = NULL, t_mediator = NULL) {
+adjust_defaults <- function(model, t1 = NULL, t2 = NULL, t_mediator = NULL,
+                            t_outcome) {
   if (is.null(t1) | is.null(t2)) {
     unique_treatments <- unique(setup_profile(model)@t_outcome[[1]])
     t1 <- unique_treatments[1, ]
@@ -36,11 +37,16 @@ adjust_defaults <- function(model, t1 = NULL, t2 = NULL, t_mediator = NULL) {
     names(t_mediator) <- c(t1[[1]], t2[[1]])
   }
 
-  list(t1 = t1, t2 = t2, t_mediator = t_mediator)
+  if (is.null(t_outcome)) {
+    t_outcome <- list(t1, t2)
+    names(t_outcome) <- c(t1[[1]], t2[[1]])
+  }
+
+  list(t1 = t1, t2 = t2, t_mediator = t_mediator, t_outcome = t_outcome)
 }
 
 direct_effect <- function(model, t1 = NULL, t2 = NULL, t_mediator = NULL) {
-  t_prep <- adjust_defaults(model, t1, t2)
+  t_prep <- adjust_defaults(model, t1, t2, t_mediator)
   t1 <- t_prep$t1
   t2 <- t_prep$t2
   t_mediator <- t_prep$t_mediator
@@ -58,15 +64,35 @@ direct_effect <- function(model, t1 = NULL, t2 = NULL, t_mediator = NULL) {
     names(t_mediator) <- seq_along(t_mediator)
   }
 
-  bind_rows(result, .id = "mediator_setting") |>
-    mutate(mediator_setting = names(t_mediator)[as.integer(mediator_setting)]) |>
-    select(outcome, mediator_setting, contrast, direct_effect)
+  bind_rows(result, .id = "indirect_setting") |>
+    mutate(indirect_setting = names(t_mediator)[as.integer(indirect_setting)]) |>
+    select(outcome, indirect_setting, contrast, direct_effect)
+}
+
+indirect_overall <- function(model, t1 = NULL, t2 = NULL, t_outcome = NULL) {
+  t_prep <- adjust_defaults(model, t1, t2, NULL, t_outcome)
+  t1 <- t_prep$t1
+  t2 <- t_prep$t2
+  t_outcome <- t_prep$t_outcome
+
+  result <- list()
+  for (i in seq_along(t_outcome)) {
+    profile1 <- setup_profile(model, t1, t_outcome[[i]])
+    profile2 <- setup_profile(model, t2, t_outcome[[i]])
+    result[[i]] <- contrast_predictions(model, profile1, profile2)[["mediators"]] |>
+      pivot_longer(everything(), names_to = "mediator", values_to = "indirect_effect") |>
+      mutate(contrast = glue("{apply(t1, 1, paste0, collapse=',')} - {apply(t2, 1, paste0, collapse=',')}"))
+  }
+
+  if (is.null(names(t_outcome))) {
+    names(t_outcome) <- seq_along(t_outcome)
+  }
+
+  bind_rows(result, .id = "direct_setting") |>
+    mutate(direct_setting = names(t_outcome)[as.integer(direct_setting)]) |>
+    select(mediator, direct_setting, contrast, indirect_effect)
 }
 
 indirect_pathwise <- function(model) {
-
-}
-
-indirect_effect <- function(model) {
 
 }
