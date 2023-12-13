12b7c5acf4d8cf8b6bec66dbad3bba6128cd4701
Kris Sankaran <sankaran.kris@gmail.com>
2023-12-13 14:12:04 -0600

    externalize the helpers

---
+++ R/lookup.R
@@ -0,0 +1,31 @@
+
+retrieve_names <- function(object, nm) {
+  object@edges |>
+    filter(node_type == nm) |>
+    pull(name)
+}
+
+#' @export
+mediators <- function(object) {
+  retrieve_names(object, "mediator")
+}
+
+#' @export
+outcomes <- function(object) {
+  retrieve_names(object, "outcome")
+}
+
+#' @export
+treatments <- function(object) {
+  retrieve_names(object, "treatment")
+}
+
+#' @export
+n_mediators <- function(object) {
+  length(mediators(object))
+}
+
+#' @export
+n_outcomes <- function(object) {
+  length(outcomes(object))
+}
