2bd1873b772190c85fbe829593813ea77807e34b
Kris Sankaran <sankaran.kris@gmail.com>
2023-12-13 14:11:18 -0600

    add a glmnet estimator

--- R/estimators.R
+++ R/estimators.R
@@ -32,130 +32,6 @@ parallelize <- function(f) {
   }
 }
 
-#' Linear Model across Responses
-lm_model <- function() {
-  new(
-    "model",
-    estimator = parallelize(lm),
-    estimates = NULL,
-    sampler = lm_sampler,
-    model_type = "lm_model()"
-  )
-}
-
-#' @importFrom dplyr bind_cols
-lm_sampler <- function(fits, new_data = NULL, indices = NULL) {
-  if (is.null(indices)) {
-    indices <- seq_along(fits)
-  }
-
-  nm <- names(fits)
-  y_hats <- list()
-  for (i in indices) {
-    sigma <- summary(fits[[i]])$sigma
-    y_ <- predict(fits[[i]], newdata = new_data)
-    y_hats[[nm[i]]] <- y_ + rnorm(length(y_), 0, sigma)
-  }
-
-  bind_cols(y_hats)
-}
-
-retrieve_names <- function(object, nm) {
-  object@edges |>
-    filter(node_type == nm) |>
-    pull(name)
-}
-
-mediators <- function(object) {
-  retrieve_names(object, "mediator")
-}
-
-outcomes <- function(object) {
-  retrieve_names(object, "outcome")
-}
-
-treatments <- function(object) {
-  retrieve_names(object, "treatment")
-}
-
-n_mediators <- function(object) {
-  length(mediators(object))
-}
-
-n_outcomes <- function(object) {
-  length(outcomes(object))
-}
-
-#' @export
-setMethod("sample", "multimedia", function(
-    x, size, pretreatment = NULL,
-    profile = NULL,
-    mediators = NULL, ...) {
-  if (missing(size)) {
-    size <- 1
-  }
-  if (is.null(profile)) {
-    profile <- setup_profile(x)
-  }
-
-  # if mediators are not provided, sample them
-  if (is.null(mediators)) {
-    mediators <- list()
-    for (i in seq_along(profile@t_mediator)) {
-      mediators[[i]] <- bind_cols(pretreatment, profile@t_mediator[[i]]) |>
-        x@mediation@sampler(x@mediation@estimates, new_data = _, i)
-    }
-    mediators <- bind_cols(mediators)
-  }
-
-  # sample outcome given everything else
-  outcomes <- list()
-  for (i in seq_along(profile@t_outcome)) {
-    outcomes[[i]] <- bind_cols(
-      pretreatment,
-      profile@t_outcome[[i]],
-      mediators
-    ) |>
-      x@outcome@sampler(x@outcome@estimates, new_data = _, i)
-  }
-
-  list(mediators = mediators, outcomes = bind_cols(outcomes))
-})
-
-#' @export
-setMethod("predict", "multimedia", function(
-    object, profile = NULL, mediators = NULL, pretreatment = NULL,
-    ...) {
-  if (is.null(profile)) {
-    profile <- setup_profile(object)
-  }
-  if (is.null(mediators)) {
-    nm <- names(profile@t_mediator)
-    mediators <- list()
-    for (i in seq_along(profile@t_mediator)) {
-      mediator_covariates <- bind_cols(pretreatment, profile@t_mediator[[i]])
-      mediators[[nm[i]]] <- predict(
-        object@mediation@estimates[[i]],
-        mediator_covariates
-      )
-    }
-    mediators <- bind_cols(mediators)
-  }
-
-  # predict outcome given everything else
-  nm <- names(profile@t_outcome)
-  outcomes <- list()
-  for (i in seq_along(profile@t_outcome)) {
-    outcomes[[nm[i]]] <- bind_cols(
-      pretreatment,
-      profile@t_outcome[[i]],
-      mediators
-    ) |>
-      predict(object@outcome@estimates[[i]], newdata = _)
-  }
-  list(mediators = mediators, outcomes = bind_cols(outcomes))
-})
-
 mediation_formula <- function(edges) {
   edges <- edges %E>%
     filter(state == "active")
@@ -189,18 +65,77 @@ outcome_formula <- function(edges) {
 #' @export
 estimate <- function(model, exper) {
   # estimate mediation model
+  data <- bind_mediation(exper)
+
+  f <- mediation_formula(model@edges)
   mediation_est <- model@mediation@estimator
-  model@mediation@estimates <- mediation_est(
-    mediation_formula(model@edges),
-    bind_mediation(exper)
-  )
+  model@mediation@estimates <- mediation_est(f, data)
 
   # estimate outcome model
+  f <- outcome_formula(model@edges)
   outcome_est <- model@outcome@estimator
-  model@outcome@estimates <- outcome_est(
-    outcome_formula(model@edges),
-    bind_mediation(exper)
-  )
+  model@outcome@estimates <- outcome_est(f, data)
+  model@treatments <- unique(exper@treatments)
 
   model
+}
+
+#' Linear Model across Responses
+lm_model <- function() {
+  new(
+    "model",
+    estimator = parallelize(lm),
+    estimates = NULL,
+    sampler = lm_sampler,
+    model_type = "lm_model()"
+  )
+}
+
+#' @importFrom dplyr bind_cols
+lm_sampler <- function(fits, new_data = NULL, indices = NULL) {
+  if (is.null(indices)) {
+    indices <- seq_along(fits)
+  }
+
+  nm <- names(fits)
+  y_hats <- list()
+  for (i in indices) {
+    sigma <- summary(fits[[i]])$sigma
+    y_ <- predict(fits[[i]], newdata = new_data)
+    y_hats[[nm[i]]] <- y_ + rnorm(length(y_), 0, sigma)
+  }
+
+  bind_cols(y_hats)
+}
+
+#' @importFrom glmnetUtils cv.glmnet.formula
+#' @importFrom glmnet cv.glmnet
+#' @export
+glmnet_model <- function(...) {
+  new(
+    "model",
+    estimator = parallelize(\(fmla, data) cv.glmnet(fmla, data, ...)),
+    estimates = NULL,
+    sampler = glmnet_sampler,
+    model_type = "glmnet_model()"
+  )
+}
+
+#' @export
+glmnet_sampler <- function(fits, new_data = NULL, indices = NULL) {
+  if (is.null(indices)) {
+    indices <- seq_along(fits)
+  }
+
+  nm <- names(fits)
+  y_hats <- list()
+  for (i in indices) {
+    lambda <- which(fits[[i]]$lambda == fits[[i]]$lambda.1se)
+    n_samples <- length(fits[[i]]$foldid)
+    sigma <- deviance(fits[[i]]$glmnet.fit)[lambda] / n_samples
+    y_ <- predict(fits[[i]], newdata = new_data)
+    y_hats[[nm[i]]] <- y_ + rnorm(length(y_), 0, sigma)
+  }
+
+  bind_cols(y_hats)
 }
