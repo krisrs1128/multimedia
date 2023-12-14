137cd0772d06dc4300a118263db3a296133d0c54
Kris Sankaran <sankaran.kris@gmail.com>
2023-12-14 11:47:02 -0600

    toy dataset mimicking police study

--- R/toy_data.R
+++ R/toy_data.R
@@ -1,7 +1,8 @@
 #' @importFrom tibble as_tibble
 random_numeric <- function(nrow, ncol, p = \(n) rnorm(n)) {
   matrix(p(nrow * ncol), nrow = nrow, ncol = ncol) |>
-    as_tibble()
+    as_tibble() |>
+    rename_with(~ str_remove(., "V"))
 }
 
 #' @importFrom glue glue
@@ -10,15 +11,29 @@ random_numeric <- function(nrow, ncol, p = \(n) rnorm(n)) {
 #' @importFrom S4Vectors SimpleList DataFrame
 #' @importFrom SummarizedExperiment SummarizedExperiment
 #' @export
-demo_joy <- function(n_samples = 100, n_taxa = 20, n_mediators = 5,
-                     n_pretreatment = 3) {
+demo_joy <- function(n_samples = 100, n_mediators = 5, n_pretreatment = 3) {
   treatment <- sample(c("Treatment", "Control"), n_samples, replace = TRUE)
   mediators <- random_numeric(n_samples, n_mediators) |>
-    rename_with(~ glue("AS{.}"))
+    rename_with(~ glue("ASV{.}"))
   outcome <- random_numeric(n_samples, 1)[[1]]
 
   SummarizedExperiment(
     assays = SimpleList(counts = t(as.matrix(mediators))),
     colData = DataFrame(treatment = treatment, PHQ = outcome)
+  )
+}
+
+#' @export
+demo_police <- function(n_samples = 100, n_outcomes = 20, n_mediators = 5) {
+  treatment <- sample(c("Treatment", "Control"), n_samples, replace = TRUE)
+  mediators <- random_numeric(n_samples, n_mediators) |>
+    rename_with(~ glue("Diet{.}"))
+  col_data <- random_numeric(n_samples, n_outcomes) |>
+    rename_with(~ glue("ASV{.}")) |>
+    bind_cols(treatment = treatment) |>
+    DataFrame()
+  SummarizedExperiment(
+    assays = SimpleList(counts = t(as.matrix(mediators))),
+    colData = col_data
   )
 }
