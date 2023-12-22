2009c775a7bf6f225ff40271cfa5de58ecf9e881
Kris Sankaran <sankaran.kris@gmail.com>
2023-12-22 00:25:27 -0800

    plotting

--- R/plot.R
+++ R/plot.R
@@ -7,15 +7,15 @@ plot_mediators <- function(indirect_effects, exper, n_panels = NULL) {
     n_panels <- min(nrow(indirect_effects), 12)
   }
   
+  p <- list()
   combined <- bind_mediation(exper)
   for (i in seq_len(n_panels)) {
-    m <- indirect$mediator[i]
-    y <- indirect$outcome[i]
-    effect <- indirect$indirect_effect[i]
+    m <- indirect_effects$mediator[i]
+    y <- indirect_effects$outcome[i]
+    effect <- indirect_effects$indirect_effect[i]
     p[[i]] <- ggplot(combined, aes(.data[[m]], .data[[y]])) +
       geom_point(aes(col = treatment), alpha = 0.7) +
-      geom_smooth(aes(col = treatment), se = FALSE, formula = y ~ x, method = "lm") +
-      labs(title = glue("IE: {round(effect, 4)}"))
+      labs(title = glue("IE: {round(effect, 2)}"))
   }
   
   wrap_plots(p) +
