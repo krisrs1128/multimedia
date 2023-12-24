de8f63e18e51928cc7d974e97e1f6cd3417a20d3
Kris Sankaran <sankaran.kris@gmail.com>
2023-12-23 18:24:56 -0800

    Calibrate across all mediator-outcome pairs

--- R/inference.R
+++ R/inference.R
@@ -57,7 +57,11 @@ bootstrap <- function(model, exper, fs = NULL, B = 100) {
     stats[[nf]] <- list()
     pb <- progress_bar$new(total = B, format = "[:bar] :current/:total ETA: :eta")
     for (b in seq_len(B)) {
+       # resample and ensure contrast t1/t2 consistency
       exper_b <- exper[sample(nrow(exper), nrow(exper), replace = TRUE), ]
+      exper_b <- exper_b[order(exper_b@treatments[[1]]), ]
+
+      # estimate model and effects
       model_b <- suppressMessages(estimate(model, exper_b))
       stats[[nf]][[b]] <- fs[[f]](model_b, exper_b)
       pb$tick()
@@ -96,27 +100,25 @@ null_contrast <- function(model, exper, nullification = "T->Y",
 
 #' @export
 fdr_summary <- function(contrast, effect = "indirect_overall", q_value = 0.15) {
-  if (effect == "indirect_overall") {
+  if (effect  == "indirect_overall") {
     fdr <- contrast |>
       group_by(source, outcome) |>
       summarise(indirect_effect = mean(indirect_effect), .group = "drop_last") |>
-      arrange(-abs(indirect_effect)) |>
-      ungroup()
+      arrange(-abs(indirect_effect))
   } else if (effect == "indirect_pathwise") {
     fdr <- contrast |>
       group_by(source, outcome, mediator) |>
       summarise(indirect_effect = mean(indirect_effect), .group = "drop_last") |>
-      arrange(-abs(indirect_effect)) |>
-      group_by(mediator)
+      arrange(-abs(indirect_effect))
   } else if (effect == "direct_effect") {
     fdr <- contrast |>
       group_by(source, outcome) |>
       summarise(direct_effect = mean(direct_effect), .groups = "drop_last") |>
-      arrange(-abs(direct_effect)) |>
-      ungroup()
+      arrange(-abs(direct_effect))
   }
 
   fdr <- fdr |>
+    ungroup() |>
     mutate(
       rank = row_number(),
       fdr_hat = cumsum(source == "synthetic") / rank
@@ -124,16 +126,15 @@ fdr_summary <- function(contrast, effect = "indirect_overall", q_value = 0.15) {
 
   cutoff <- fdr |>
     filter(fdr_hat < q_value) |>
-    summarise(ix = max(rank))
+    summarise(ix = max(rank)) |>
+    pull(ix)
 
-  if (effect == "indirect_pathwise") {
+  if (length(cutoff) == 0) {
     fdr <- fdr |>
-      left_join(cutoff) |>
-      mutate(keep = rank < ix) |>
-      select(-ix)
+      mutate(keep = FALSE)
   } else {
     fdr <- fdr |>
-      mutate(keep = rank < cutoff$ix[1])
+      mutate(keep = rank < cutoff)
   }
   fdr
 }
