ad61cc93b19222eaafac4fc129e97324899c16dc
Kris Sankaran <sankaran.kris@gmail.com>
2024-08-20 18:43:11 -0500

    tests for sensitivity analysis

--- tests/testthat/test-sensitivity.R
+++ tests/testthat/test-sensitivity.R
@@ -1,14 +1,27 @@
+library(multimedia)
 
 xy_data <- demo_spline()
 exper <- mediation_data(xy_data, starts_with("outcome"), "treatment", "mediator")
 model <- multimedia(exper, outcome_estimator = glmnet_model(lambda = 1e-2)) |>
   estimate(exper)
+indirect_overall(model)
 
-sensitivity_curve <- sensitivity(model, exper, n_rho = 10)
+sensitivity_curve <- sensitivity(model, exper, n_rho = 3, n_bootstrap = 10)
 test_that("Sensitivity curve has correct bounds.", {
-  expect_equal(nrow(sensitivity_curve), 10 * n_outcomes(model))
+  expect_equal(nrow(sensitivity_curve), 3 * n_outcomes(model))
 })
 
 test_that("Sensitivity curve has correct column names.", {
-  expect_named(sensitivity_curve, c("outcome", "rho", "indirect_overall"))
+  expect_named(sensitivity_curve, c("outcome", "rho", "indirect_effect", "indirect_effect_standard_error"))
+})
+
+test_that("Sensitivity at rho = 0 agrees with original indirect effect", {
+  sensitivity0 <- sensitivity_curve |>
+    filter(rho == 0)
+
+  indirect_diff <- indirect_overall(model) |>
+    effect_summary() |>
+    left_join(sensitivity0, by = "outcome") |>
+    mutate(difference = abs(indirect_effect.x - indirect_effect.y))
+  expect_true(all(indirect_diff$difference < 0.05))
 })
