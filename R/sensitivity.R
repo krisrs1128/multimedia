94b76e0de33b0c8d7214f5263a7a5ef4e2b7f6dc
Kris Sankaran <sankaran.kris@gmail.com>
2024-08-27 15:26:20 -0500

    generalization in preparation for perturbation strategy

--- R/sensitivity.R
+++ R/sensitivity.R
@@ -42,28 +42,25 @@
 #' @importFrom tidyselect ends_with
 #' @importFrom stats sd
 #' @noRd
-sensitivity_subset <- function(summarization, model, exper,
-                               confound_ix = NULL, rho_seq = NULL, 
+sensitivity_factory <- function(summarization, model, exper, sampler, sensitivity_seq = NULL,
                                n_bootstrap = 100, progress = TRUE) {
   model_types <- c(model@mediation@model_type, model@outcome@model_type)
   supported_models <- c("rf_model()", "lm_model()", "glmnet_model()")
   if (!all(model_types %in% supported_models)) {
     cli_abort("Sensitivity analysis is only supported for models of type lm_model(), glmnet_model(), and rf_model().")
   }
-  if (is.null(rho_seq)) {
-    rho_seq <- seq(-0.9, 0.9, by = 0.2)
-  }
 
   sensitivity_curve <- list()
   k <- 1
   pb <- progress_bar$new(
-    total = length(rho_seq) * n_bootstrap, 
+    total = length(sensitivity_seq) * n_bootstrap,
     format = "[:bar] :current/:total ETA: :eta"
   )
-  for (i in seq_along(rho_seq)) {
+  for (i in seq_along(sensitivity_seq)) {
     for (b in seq_len(n_bootstrap)) {
       exper_ <- exper[sample(nrow(exper)), ]
-      samples <- sensitivity_sample(model, exper, confound_ix, rho_seq[i])
+      samples <- sampler(model, exper, sensitivity_seq[i])
+
       t_ <- model@treatments
       for (j in seq_len(nrow(t_))) {
         ix <- apply(exper_@treatments, 1, \(x) x == t_[j, ])
@@ -73,14 +70,14 @@ sensitivity_subset <- function(summarization, model, exper,
 
       sensitivity_curve[[k]] <- estimate(model, exper_) |>
         summarization() |>
-        mutate(rho = rho_seq[i], bootstrap = b)
+        mutate(perturbation = sensitivity_seq[i], bootstrap = b)
       k <- k + 1
       if (progress) pb$tick()
     }
   }
 
   bind_rows(sensitivity_curve) |>
-    group_by(.data$outcome, .data$rho) |>
+    group_by(.data$outcome, .data$perturbation) |>
     summarise(across(ends_with("effect"), c(`_` = mean, standard_error = sd))) |>
     rename_with(~ gsub("__$", "", .))
 }
@@ -119,10 +116,20 @@ sensitivity_subset <- function(summarization, model, exper,
 #' @export
 sensitivity <- function(model, exper, confound_ix = NULL, rho_seq = NULL, 
                         n_bootstrap = 100, progress = TRUE) {
-  (\(x) indirect_overall(x) |>
-    effect_summary()) |>
-    sensitivity_subset(model, exper, confound_ix, rho_seq, n_bootstrap, 
-                       progress)
+  if (is.null(rho_seq)) {
+    rho_seq <- seq(-0.9, 0.9, by = 0.2)
+  }
+
+  summarization <- \(x) {
+    indirect_overall(x) |>
+      effect_summary()
+  }
+
+  sampler <- \(model, exper, rho) {
+    sensitivity_subset_sample(model, exper, confound_ix, rho)
+  }
+  sensitivity_factory(summarization, model, exper, sampler, rho_seq, n_bootstrap, progress) |>
+    rename(rho = "perturbation")
 }
 
 #' Sensitivity Analysis for Pathwise Indirect Effects
@@ -160,10 +167,15 @@ sensitivity <- function(model, exper, confound_ix = NULL, rho_seq = NULL,
 sensitivity_pathwise <- function(model, exper, confound_ix = NULL, 
                                  rho_seq = NULL, n_bootstrap = 100,  
                                  progress = TRUE) {
-  (\(x) indirect_pathwise(x) |>
-    effect_summary()) |>
-    sensitivity_subset(model, exper, confound_ix, rho_seq, n_bootstrap, 
-                       progress)
+  summarization <- \(x) {
+    indirect_pathwise(x) |>
+      effect_summary()
+  }
+  sampler <- \(model, exper, rho) {
+    sensitivity_subset_sample(model, exper, confound_ix, rho)
+  }
+  sensitivity_factory(summarization, model, exper, sampler, rho_seq, n_bootstrap, progress) |>
+    rename(rho = "perturbation")
 }
 
 #' @importFrom dplyr arrange across everything
@@ -196,13 +208,19 @@ sorted_treatments <- function(model) {
 #'   estimate(exper)
 #' multimedia:::sensitivity_sample(model, exper)
 #' @noRd
-sensitivity_sample <- function(model, exper, confound_ix = NULL, rho = 0.0) {
+sensitivity_subset_sample <- function(model, exper, confound_ix = NULL, rho = 0.0) {
   Nm <- n_mediators(model)
   Ny <- n_outcomes(model)
   epsilon <- covariance_matrix(model, confound_ix, rho) |>
     mvrnorm(nrow(exper), rep(0, Nm + Ny), Sigma = _)
+  sensitivity_sample(model, exper, epsilon)
+}
 
+sensitivity_sample <- function(model, exper, epsilon) {
   t_ <- model@treatments
+  Nm <- n_mediators(model)
+  Ny <- n_outcomes(model)
+
   samples <- list()
   for (i in seq_len(nrow(t_))) {
     exper_ <- bind_mediation(exper)
@@ -272,5 +290,5 @@ covariance_matrix <- function(model, confound_ix = NULL, rho = 0.0) {
   ecov$vectors %*% diag(ecov$values) %*% t(ecov$vectors)
 }
 
-sensitivity_perturb <- function(model, exper, covariance, nu_seq = NULL, n_bootstrap = 100, progress = TRUE) {
-}
+# sensitivity_perturb <- function(model, exper, perturb, nu_seq = NULL, n_bootstrap = 100, progress = TRUE) {
+# }
