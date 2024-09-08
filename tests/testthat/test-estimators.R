c443796d4bd2bf40e486e00efd2b0d2a3a8aaecf
Kris Sankaran <sankaran.kris@gmail.com>
2024-09-07 22:24:57 -0500

    add more tests

--- tests/testthat/test-estimators.R
+++ tests/testthat/test-estimators.R
@@ -22,3 +22,17 @@ test_that("Estimation with glmnet works in a simple case", {
     expect_equal(colnames(predictions$mediators), mediators(model))
     expect_equal(mediators(model), paste0("ASV", 1:5))
 })
+
+test_that("Can compile and estimate a BRMS model.", {
+    model <- multimedia(exper, brms_model())
+    expect_equal(outcome_model(model)@model_type, "brms_model()")
+    model <- estimate(model, exper)
+    expect_s4_class(model, "multimedia")
+    y_hat <- predict(model)
+    expect_named(y_hat, c("mediators", "outcomes"))
+    expect_named(y_hat$mediators, paste0("ASV", 1:5))
+
+    samples <- multimedia:::brms_sampler(model@outcome@estimates)
+    expect_named(samples, "PHQ")
+    expect_equal(dim(samples), c(100, 1))
+})
